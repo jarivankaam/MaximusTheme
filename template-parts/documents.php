f6a10d5113f5cfd85bda9ac1efdfd5d20688e734
Az3rAp1 <jjj.vankaam@student.avans.nl>
2025-05-30 13:03:28 +0200

    add documents code

--- template-parts/documents.php
+++ template-parts/documents.php
@@ -1,5 +1,11 @@
 <?php if( have_rows('files') ): ?>
     <div class="container">
+        <?php $title = get_sub_field('title'); ?>
+        <?php if ($title): ?>
+            <div class="section-heading">
+                <h2><?= esc_html($title) ?></h2>
+            </div>
+        <?php endif; ?>
         <div class="row">
             <div class="col-12">
                 <ul class="list-unstyled">
@@ -7,21 +13,34 @@
                     // Loop through each row of the 'files' repeater
                     while( have_rows('files') ): the_row();
 
-                        // Get the sub-field (File field set to return Array)
-                        $file = get_sub_field('file');
+                        // Check the true/false 'location' sub-field
+                        $is_file = get_sub_field('location');
 
-                        // Make sure we have a valid file array
-                        if(
-                            is_array($file) &&
-                            ! empty($file['url']) &&
-                            ! empty($file['filename'])
-                        ) : ?>
-                            <li>
-                                <a href="<?php echo esc_url( $file['url'] ); ?>" download>
-                                    <?php echo esc_html( $file['filename'] ); ?>
-                                </a>
-                            </li>
-                        <?php endif;
+                        if( $is_file ):
+                            // If location is true, treat 'file' as an ACF File (Array)
+                            $file = get_sub_field('file');
+                            if( is_array($file) && ! empty($file['url']) && ! empty($file['filename']) ): ?>
+                                <li>
+                                    <a href="<?php echo esc_url( $file['url'] ); ?>" download>
+                                        <?php echo esc_html( $file['filename'] ); ?>
+                                    </a>
+                                </li>
+                            <?php
+                            endif;
+
+                        else:
+                            // If location is false, treat 'link' as a simple URL text field
+                            $link = get_sub_field('link');
+                            if( ! empty($link) ): ?>
+                                <li>
+                                    <a href="<?php echo esc_url( $link ); ?>" target="_blank" rel="noopener">
+                                        <?php echo esc_html( $link ); ?>
+                                    </a>
+                                </li>
+                            <?php
+                            endif;
+
+                        endif;
 
                     endwhile; ?>
                 </ul>
@@ -29,5 +48,5 @@
         </div>
     </div>
 <?php else: ?>
-    <p>No files found.</p>
+    <p>No items found.</p>
 <?php endif; ?>
