ffdb0359100903f4ecd4fb5f95c2d8e11873028b
jarivankaam <jari@jarivankaam.nl>
2026-03-21 11:37:01 +0100

    fix

--- functions/send_email_hook.php
+++ functions/send_email_hook.php
@@ -44,8 +44,9 @@ function notify_users_on_page_update($post_id)
 
     // Find all users who have the checkbox ticked (value stored as '1')
     $users = get_users(array(
-        'meta_key' => $um_meta_key,
-        'meta_value' => '1',
+        'meta_key' => 'notify',
+        'meta_value' => '"Ja"',
+        'meta_compare' => 'LIKE',
         'fields' => array('ID', 'user_email', 'display_name'),
     ));
 
